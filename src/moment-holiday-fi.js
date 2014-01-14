06d7bd45e75fd2583c256bf7bfbcb948e871e7dc
Sampo Toiva <sampo.toiva@gmail.com>
2014-01-14 14:20:32 +0200

    Rename main file.

--- src/moment-holiday-fi.js
+++ src/moment-holiday-fi.js
@@ -20,13 +20,13 @@
     moment = typeof require !== "undefined" && require !== null ? require("moment") : this.moment;
 
     //Static variables
-    var _saturday = 6, 
-        _november = 9, 
-        _december = 10,
-        _june = 5;
+    var saturday = 6, 
+        november = 9, 
+        december = 10,
+        june = 5;
 
     //Holiday definitions, static dates
-    var _holidays = {
+    var holidays = {
     'M': {//Day, Month
             '01/01': "Uudenvuodenpäivä",
             '06/01': "Loppiainen",
@@ -37,44 +37,44 @@
 
     };
 
-    var _staticHolidays = function(moment){
-        return _holidays['M'][moment.format('DD/MM')];
+    var staticHolidays = function(moment){
+        return holidays['M'][moment.format('DD/MM')];
     };
 
     //variable dates
     //Pyhäinpäivä 31.10-6.11 lauantaina
-     var _allSaintsDay = function(moment){
-        var start = moment.clone().month(_saturday).date(31);
-        var end = moment.clone().month(_november).date(30);
+     var allSaintsDay = function(moment){
+        var start = moment.clone().month(saturday).date(31);
+        var end = moment.clone().month(november).date(30);
      
-        if(_isSaturdayHoliday(moment, start, end)){
+        if(isSaturdayHoliday(moment, start, end)){
             return "Pyhäinpäivä";
         }
     };
 
     //Juhannus 20.6.-26.6. lauantaina
-    var _midSummer = function(moment){
-        var start = moment.clone().month(_june).date(20);
-        var end = moment.clone().month(_june).date(26);
+    var midSummer = function(moment){
+        var start = moment.clone().month(june).date(20);
+        var end = moment.clone().month(june).date(26);
 
-        if(_isSaturdayHoliday(moment, start, end)){
+        if(isSaturdayHoliday(moment, start, end)){
             return "Juhannuspäivä";
         }
     };
 
-    var _isSaturdayHoliday = function(moment, start, end){
-        return _isSaturday(moment) && moment.isAfter(start) && moment.isBefore(end);
+    var isSaturdayHoliday = function(moment, start, end){
+        return isSaturday(moment) && moment.isAfter(start) && moment.isBefore(end);
     };
 
-    var _isSaturday = function(moment){
-        return moment.day() === _saturday;
+    var isSaturday = function(moment){
+        return moment.day() === saturday;
     };
 
     /**
     /*  @param year The year for which the Easter sunday is calculated
-    /*  @return easter sunday json: {day: <day_of_month>, month: <month>}
+    /*  @return easter sunday json: {day: <dayofmonth>, month: <month>}
     **/ 
-    var _easterSunday = function(year) {
+    var easterSunday = function(year) {
         var a = Math.floor(year % 19),
             b = Math.floor(year / 100),
             c = Math.floor(year % 100),
@@ -94,8 +94,8 @@
     };
 
 
-    var _easterRelatedDays = function(moment) {
-        var easterSunday = _easterSunday(moment.year()),
+    var easterRelatedDays = function(moment) {
+        var easterSunday = easterSunday(moment.year()),
             goodFriday = easterSunday.clone().subtract("days", 2),
             easterMonday = easterSunday.clone().add("days", 1),
             ascension = easterSunday.clone().add("days", 39);
@@ -113,10 +113,10 @@
 
     moment.fn.holiday = function() {
         var currentDate = this.format('DD/MM');
-        return _staticHolidays(this) || 
-               _easterRelatedDays(this) ||
-               _midSummer(this) ||
-               _allSaintsDay(this);
+        return staticHolidays(this) || 
+               easterRelatedDays(this) ||
+               midSummer(this) ||
+               allSaintsDay(this);
     };
  
     if ((typeof module !== "undefined" && module !== null ? module.exports : void 0) != null) {
